434623d4b26a3b70926cfe06487a7b1c5b62b96b
Fujiyoshi <fujita@fsapple2195.fi.f-secure.com>
2020-06-19 08:53:05 +0900

    added for ptlab

--- x1.go
+++ x1.go
@@ -1,5 +1,22 @@
 package main
+// #cgo LDFLAGS: ./attack.so
+// typedef int (*intFunc) ();
+//
+// int
+// bridge_int_func(intFunc f)
+// {
+//      return f();
+// }
+//
+// int fortytwo()
+// {
+//      return 42;
+// }
+import "C"
 import "fmt"
+
 func main() {
-    fmt.Println("Hello, World")
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)))
+    // Output: 42
 }
