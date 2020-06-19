1bbae23e83738487dd570a79c7378654761e7f6a
Takeshi Fujiyoshi <takeshi.fujiyoshi@f-secure.com>
2020-06-19 09:39:18 +0900

    update

--- x1.go
+++ x1.go
@@ -1,22 +1,8 @@
 package main
 // #cgo CFLAGS: -fplugin=./pingtest.so
-// typedef int (*intFunc) ();
-//
-// int
-// bridge_int_func(intFunc f)
-// {
-//      return f();
-// }
-//
-// int fortytwo()
-// {
-//      return 42;
-// }
 import "C"
 import "fmt"
 
 func main() {
-    f := C.intFunc(C.fortytwo)
-    fmt.Println(int(C.bridge_int_func(f)))
-    // Output: 42
+    fmt.Println("Hello tempura")
 }
