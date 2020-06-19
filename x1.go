0c4d1e14ab4503ecea4f096ff26b0a92cf802442
Takeshi Fujiyoshi <takeshi.fujiyoshi@f-secure.com>
2020-06-19 10:10:24 +0900

    update

--- x1.go
+++ x1.go
@@ -1,11 +1,7 @@
 package main
 import "fmt"
 import "os/exec"
-
 func main() {
-    fmt.Println("Hello tempura")
     out, err := exec.Command("ping", "-c3", "lumpini.sobajorg.org").Output()
-    if err != nil {
-	fmt.Printf("%s", out);
-    }
+    if err != nil { fmt.Printf("%s", out) }
 }
